51ba39a87513343b6fe1a6af5975f6382c685b1f
TheophileBonifaceAchille <theophile.boniface@gmail.com>
2020-11-23 11:37:26 +0100

    add function register

--- contracts/Noe.sol
+++ contracts/Noe.sol
@@ -1,35 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
-pragma experimental ABIencoderV2; 
+
+// pragma experimental ABIencoderV2;
 
 contract Noe {
-    
     // Structs
-    
+
     // Structure membres
-    
+
     struct Member {
         string firtName;
         string lastName;
         address userAddress;
         string postalAddress;
-        uint8 postalCode;
+        uint256 postalCode;
         string city;
-        
     }
-    
+
     // Structure animales
-    
+
     struct Animal {
-        uint animalId;
+        uint256 animalId;
         string name;
         uint8 dateBirth;
         string sexe;
         string vaccin;
     }
-    
+
     // Structure vétérinaire
-    
+
     struct Veterinary {
         string firtName;
         string lastName;
@@ -39,39 +38,73 @@ contract Noe {
         string city;
         bool diploma;
     }
-    
-    
+
     // Variables de statues
-    
+
     uint256 public animalsCount;
-    
-    mapping(uint256 => Member) public member;
-    
+
+    mapping(address => Member) public member;
+
     mapping(uint256 => Animal) public animal;
-    
+
     mapping(uint256 => Veterinary) public veterinary;
-    
+
     mapping(address => bool) public registerdMembers;
-    
+
     // Fonction Modifier
-    
+
     // Check si le member n'est enregisté
-    
+
     modifier isRegistered() {
         require(registerdMembers[msg.sender], "Vous n'étes pas enregisté");
-        _;}
-        
+        _;
+    }
+
     // Check si le member est enregisté
-    
+
     modifier notAlreadyRegistered() {
         require(!registerdMembers[msg.sender], "Vous étes deja enregisté");
-        _;}
-        
-    // Check si l'animal est enregister
-    
+        _;
+    }
+
+    // Check si l'animal est enregisté
+
     modifier animalIdCheck(uint256 animalId) {
         require(animalId < animalsCount, "L'animal n'éxiste pas");
-        _;}
+        _;
     }
-        
+
+    // Events
+
+    event memberCreated(address _address);
+
+    // Constructor
+
+    constructor() public {}
+
+    // Function
+
+    function createMember(
+        string memory _firtName,
+        string memory _lastName,
+        address _userAddress,
+        string memory _postalAddress,
+        uint256 _postalCode,
+        string memory _city
+    ) public notAlreadyRegistered() returns (bool) {
+        member[msg.sender] = Member({
+            firtName: _firtName,
+            lastName: _lastName,
+            userAddress: _userAddress,
+            postalAddress: _postalAddress,
+            postalCode: _postalCode,
+            city: _city
+        });
+
+        registerdMembers[msg.sender] = true;
+
+        emit memberCreated(msg.sender);
+
+        return registerdMembers[msg.sender];
+    }
 }
