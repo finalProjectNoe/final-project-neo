ad040eea3a9039f5185964de25239de1fdc18ac3
Miklsan <mborghmans@gmail.com>
2020-11-23 11:50:19 +0100

    add struct to Noe.sol as ERC721 with contracts import

--- contracts/Noe.sol
+++ contracts/Noe.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.6.0;
 
 // pragma experimental ABIencoderV2;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Noe {
+contract Noe is ERC721 {
     // Structs
-
+constructor() public ERC721("Noe", "NOE") {}
     // Structure membres
 
     struct Member {
